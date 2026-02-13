7c179a2cec0ddfef11e4db9cc1f268123ba743cb
Igor Oliveira <schreiber2011@gmail.com>
2026-02-13 02:40:58 -0400

    feat: adding user entity

---
+++ src/Backend/MyRecipeBook.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyRecipeBook.Domain.Entities;
+
+[Table("users")]
+public class User : EntiyBase
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
