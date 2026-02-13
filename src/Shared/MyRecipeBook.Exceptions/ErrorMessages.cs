86e06c1c3b218e2e9bbfcc7afe34af1c87db0d39
Igor Oliveira <schreiber2011@gmail.com>
2026-02-13 02:07:32 -0400

    feat: adding exceptions and messages to registered users

---
+++ src/Shared/MyRecipeBook.Exceptions/ErrorMessages.cs
@@ -0,0 +1,19 @@
+﻿namespace MyRecipeBook.Exceptions;
+
+public class ErrorMessages
+{
+    // Templates
+    private static string EMPTY => ResourceMessagesException.EMPTY;
+    private static string INVALID => ResourceMessagesException.INVALID;
+    private static string NAME => ResourceMessagesException.NAME;
+    private static string EMAIL => ResourceMessagesException.EMAIL;
+    private static string PASSWORD => ResourceMessagesException.PASSWORD;
+
+    public static string PASSWORD_NOT6CHAR => ResourceMessagesException.PASSWORD_NOT6CHAR;
+
+    public static string PASSWORD_EMPTY => string.Format(EMPTY, PASSWORD);
+    public static string NAME_EMPTY => string.Format(EMPTY, NAME);
+    public static string EMAIL_EMPTY => string.Format(EMPTY, EMAIL);
+    public static string EMAIL_INVALID => string.Format(INVALID, EMAIL);
+
+}
