2688a35da7d5ba69df51f1440c54973bf6697e7b
Igor Oliveira <schreiber2011@gmail.com>
2026-02-13 03:19:51 -0400

    feat: adding user repository

---
+++ src/Backend/MyRecipeBook.Domain/Repositories/User/IUserReadOnlyRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace MyRecipeBook.Domain.Repositories.User;
+
+public interface IUserReadOnlyRepository
+{
+    Task<bool> ExistsActiveUserWithEmail(string email);
+}
---
+++ src/Backend/MyRecipeBook.Domain/Repositories/User/IUserWriteOlnyRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace MyRecipeBook.Domain.Repositories.User;
+
+public interface IUserWriteOlnyRepository
+{
+    public Task Add(Entities.User user);
+}
