38da6b5bec4e8ad268acc958bccfb22a20727c42
Igor Oliveira <schreiber2011@gmail.com>
2026-02-13 03:15:24 -0400

    feat: adding support classes

---
+++ src/Backend/MyRecipeBook.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+using System.Net;
+
+namespace MyRecipeBook.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if(context.Exception is MyRecipeBookException)
+        {
+            HandleProjectException(context);
+        }
+        else
+        {
+            ThrowUnknowException(context);
+        }
+    }
+
+    public void HandleProjectException(ExceptionContext context)
+    {
+        if(context.Exception is ErrorOnValidationException)
+        {
+            var validationException = context.Exception as ErrorOnValidationException;
+            context.HttpContext.Response.StatusCode = (int) HttpStatusCode.BadRequest;
+            context.Result = 
+                new BadRequestObjectResult(new ResponseErrorJson(validationException.ErrorMessages));
+        }
+    }
+
+    public void ThrowUnknowException(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Result =
+            new ObjectResult(new ResponseErrorJson("kabum"));
+    }
+}
