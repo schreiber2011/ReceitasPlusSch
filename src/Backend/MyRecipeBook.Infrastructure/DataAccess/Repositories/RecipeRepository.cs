f6377513eb502f6be3b9674c66750d9b2e93c13d
Igor Oliveira <schreiber2011@gmail.com>
2026-03-04 18:27:08 -0400

    feat: adding recipe register

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/RecipeRepository.cs
@@ -0,0 +1,10 @@
+﻿using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Repositories.Recipe;
+
+namespace MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+public class RecipeRepository(MyrecipeBookDbContext dbContext)
+    : IRecipeWriteOnlyRepository
+{
+    public async Task Add(Recipe recipe) => await dbContext.Recipes.AddAsync(recipe);
+}
