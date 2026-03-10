7c212aa2565de90032ec73cf29a4b9517dda1628
Igor Oliveira <schreiber2011@gmail.com>
2026-03-09 23:05:37 -0400

    feat: adding post to get a filtered list of recipes

---
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/Filter/FilterRecipeUseCase.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Services.LoggedUser;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.Filter;
+
+public class FilterRecipeUseCase(
+    IMapper mapper,
+    ILoggedUser loggedUser) : IFilterRecipeUseCase
+{
+
+    public async Task<ResponseRecipesJson> Execute(RequestFilterRecipeJson request)
+    {
+        Validate(request);
+
+        var loggeduser = await loggedUser.User();
+
+        return new ResponseRecipesJson
+        {
+            Recipes = []
+        };
+    }
+
+    private static void Validate(RequestFilterRecipeJson request)
+    {
+        var validator = new FilterRecipeValidator();
+
+        var result = validator.Validate(request);
+
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(
+                error => error.ErrorMessage).Distinct().ToList();
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
